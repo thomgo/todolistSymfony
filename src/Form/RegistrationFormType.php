eca7302ec35f088c56e5728215b2bd09252649fe
Thomas <tg.59290@gmail.com>
2020-11-26 15:33:33 +0100

    add style for login and register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,29 +12,44 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("firstname")
-            ->add("lastname")
-            ->add("age")
-            ->add("position")
-            ->add('email')
+            ->add("firstname", TextType::class, [
+                "label" => "Prénom"
+            ])
+            ->add("lastname", TextType::class, [
+                "label" => "Nom"
+            ])
+            ->add("age", NumberType::class, [
+                "label" => "Age",
+                'html5' => true,
+                "scale" => 0
+            ])
+            ->add("position", TextType::class, [
+                "label" => "Poste occupé"
+            ])
+            ->add('email', EmailType::class, [
+                "label" => ""
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                "label" => "Mot de passe",
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => "Merci d'indiquer un mot de passe",
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit faire au minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
@@ -42,6 +57,12 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('save', SubmitType::class, [
                 'label' => "S'enregistrer",
+                "attr" => [
+                    "class" => "btn btn-lg secondBg my-3 text-white"
+                ],
+                'row_attr' => [
+                    'class' => 'text-center'
+                ]
             ])
         ;
     }
